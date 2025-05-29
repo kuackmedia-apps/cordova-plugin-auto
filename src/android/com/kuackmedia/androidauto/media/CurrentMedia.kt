828287ad8b6af33050b236dc3ab5c85c37b63b8d
Federico Mac Eachen <federico.maceachen@halopowered.com>
2025-05-29 16:57:48 -0300

    Fix imports

--- src/android/com/kuackmedia/androidauto/media/CurrentMedia.kt
+++ src/android/com/kuackmedia/androidauto/media/CurrentMedia.kt
@@ -1,19 +1,8 @@
 package com.kuackmedia.androidauto.media
 
-import android.content.Context
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.util.Log
-import com.kuackmedia.androidauto.models.EmptyModel
-import com.kuackmedia.androidauto.models.MediaItem
-import com.kuackmedia.androidauto.models.QueueItem
-import com.kuackmedia.androidauto.tree.MediaItemFactory
-import com.kuackmedia.androidauto.tree.MediaItemJsonAdapter
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import java.io.File
 
 object CurrentMedia {
   const val TAG = "CurrentMedia"
